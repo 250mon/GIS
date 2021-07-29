42d4a9b099dbb3ae89546afe07a3775938f98838
jj <jj@example.com>
2021-07-29 21:18:48 +0900

    .

--- census.py
+++ census.py
@@ -2,9 +2,10 @@ import numpy as np
 import pandas as pd
 import os
 
-dir_name = 'E:/Datasets/PeopleDong'
-src_file_path = 'E:/Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간.csv'
-dst_file_path = 'E:/Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간_extr.csv'
+# dir_name = 'E:'
+dir_name = '/home/jy/Downloads'
+src_file_path = os.path.join(dir_name, 'Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간.csv')
+dst_file_path = os.path.join(dir_name, 'Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간_extr.csv')
 census_df = pd.read_csv(src_file_path, encoding='euc-kr')
 
 # The first 14 columns selected
@@ -21,14 +22,27 @@ def get_code(str_code):
     adm_div_gprs = code_regex.search(str_code)
     return adm_div_gprs.group(2)
 
+def get_name(str_code):
+    code_regex = re.compile(r'([^()]+)\((\d+)00\)')
+    adm_div_gprs = code_regex.search(str_code)
+    emd_name = adm_div_gprs.group(1)
+    emd_base = emd_name.split(' ', maxsplit=1)[1]
+    return emd_base
+
 # census_df_final = census_df_sub2.iloc[:, [0]].apply(get_code): not working
 # because it works like get_code(Series)
-census_df_final['EMD_CODE'] = census_df_final.iloc[:, 0].apply(get_code)
+# census_df_final['EMD_CODE'] = census_df_final.iloc[:, 0].apply(get_code)
+# census_df_final['EMD_NAME'] = census_df_final['Adm_Div'].apply(get_name)
+new_df = census_df_final['Adm_Div'].str.split(' ', n=1, expand=True)
 census_df_final = census_df_final.drop('Adm_Div', axis=1)
+census_df_final['SIDO_NAME'] = new_df[0]
+census_df_final['EMD_NAME'] = new_df[1]
 
-census_df_final.to_csv(dst_file_path, index=False, encoding='euc-kr')
-
-# pd.set_option('display.max_columns', None)
+pd.set_option('display.max_columns', None)
 # census_df_sub = census_df.loc[:, ['EMD_CD', 'Total_Living_Pop']]
 # print(census_df_sub.groupby(by='EMD_CD').describe())
+print(census_df_final['SIDO_NAME', 'EMD_NAME'])
+# print(census_df_final.loc[census_df_final.duplicated('EMD_NAME'), ['EMD_NAME']])
 
+# Save
+# census_df_final.to_csv(dst_file_path, index=False, encoding='euc-kr')
--- read_shp.py
+++ read_shp.py
@@ -1,7 +1,20 @@
 import os
 import shapefile
+import pandas as pd
 
-shp_path = "E:/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
+# shp_path = "E:/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
+shp_path = "/home/jy/Downloads/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
 assert os.path.exists(shp_path)
 
-with shapefile.Reader(shp_path) as shpfile:
+record_dicts_list = []
+with shapefile.Reader(shp_path, encoding='euc-kr') as sf:
+    print(sf.fields)
+    for rec in sf.records():
+        if rec['EMD_CD'].startswith('11'):
+            record_dicts_list.append(rec.as_dict())
+
+records_df = pd.DataFrame(record_dicts_list)
+duplicate_nm_df = records_df[records_df.duplicated('EMD_KOR_NM')]
+# print(duplicate_nm_df)
+dup_df = records_df[records_df['EMD_KOR_NM'].isin(duplicate_nm_df['EMD_KOR_NM'])]
+print(dup_df)
