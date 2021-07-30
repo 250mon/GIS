f87e8dca862d4ed729bc73220e6476cb8cdd5730
250mon <lambki@naver.com>
2021-07-30 12:49:09 +0900

    extract code, emd from adm_div in census

--- census.py
+++ census.py
@@ -2,8 +2,8 @@ import numpy as np
 import pandas as pd
 import os
 
-# dir_name = 'E:'
-dir_name = '/home/jy/Downloads'
+dir_name = 'E:/'
+# dir_name = '/home/jy/Downloads'
 src_file_path = os.path.join(dir_name, 'Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간.csv')
 dst_file_path = os.path.join(dir_name, 'Datasets/Administrative/행안부/행안부인구/202106_202106_연령별인구현황_월간_extr.csv')
 census_df = pd.read_csv(src_file_path, encoding='euc-kr')
@@ -14,35 +14,25 @@ census_df_sub = census_df.iloc[:, list(range(14))]
 col_name = ['Adm_Div', 'Total', 'Total2', 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 census_df_sub.columns = col_name
 # 'Total2' dropped, because it is a duplicate of 'Total'
-census_df_final = census_df_sub.drop('Total2', axis=1)
+census_df_sub = census_df_sub.drop('Total2', axis=1)
+
+# Splitting Adm_Div
 # 'Adm_Div': 서울특별시... (code 10 digits)
-import re
-def get_code(str_code):
-    code_regex = re.compile(r'([^()]+)\((\d+)00\)')
-    adm_div_gprs = code_regex.search(str_code)
-    return adm_div_gprs.group(2)
-
-def get_name(str_code):
-    code_regex = re.compile(r'([^()]+)\((\d+)00\)')
-    adm_div_gprs = code_regex.search(str_code)
-    emd_name = adm_div_gprs.group(1)
-    emd_base = emd_name.split(' ', maxsplit=1)[1]
-    return emd_base
-
-# census_df_final = census_df_sub2.iloc[:, [0]].apply(get_code): not working
-# because it works like get_code(Series)
-# census_df_final['EMD_CODE'] = census_df_final.iloc[:, 0].apply(get_code)
-# census_df_final['EMD_NAME'] = census_df_final['Adm_Div'].apply(get_name)
-new_df = census_df_final['Adm_Div'].str.split(' ', n=1, expand=True)
+# using str.extract
+admdiv_regex = "(?P<SIDO>[가-햫]+)\s(?P<EMD>[^()]+)\((?P<EMD_CODE>\d+)00\)"
+admdiv_df = census_df_sub['Adm_Div'].str.extract(admdiv_regex, expand=True)
+# concat
+census_df_admdiv = pd.concat([census_df_sub, admdiv_df], axis=1)
+# filtering headers like 시, 구
+census_df_final = census_df_admdiv[census_df_admdiv['EMD'].str.split().str.len() > 1]
 census_df_final = census_df_final.drop('Adm_Div', axis=1)
-census_df_final['SIDO_NAME'] = new_df[0]
-census_df_final['EMD_NAME'] = new_df[1]
 
-pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+# pd.set_option('display.max_columns', None)
 # census_df_sub = census_df.loc[:, ['EMD_CD', 'Total_Living_Pop']]
-# print(census_df_sub.groupby(by='EMD_CD').describe())
-print(census_df_final['SIDO_NAME', 'EMD_NAME'])
-# print(census_df_final.loc[census_df_final.duplicated('EMD_NAME'), ['EMD_NAME']])
+grouped = census_df_final.groupby(by='SIDO')
+# print(grouped.count())
+print(grouped.get_group('서울특별시').describe())
 
 # Save
-# census_df_final.to_csv(dst_file_path, index=False, encoding='euc-kr')
+census_df_final.to_csv(dst_file_path, index=False, encoding='euc-kr')
--- read_shp.py
+++ read_shp.py
@@ -2,7 +2,30 @@ import os
 import shapefile
 import pandas as pd
 
-# shp_path = "E:/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
+# 통계청_행정동경계
+shp_path = "E:/Datasets/Administrative/통계청_행정동경계_202006/Z_SOP_BND_ADM_DONG_PG.dbf"
+assert os.path.exists(shp_path)
+
+record_dicts_list = []
+with shapefile.Reader(shp_path, encoding='euc-kr') as sf:
+    print(sf.fields)
+    for rec in sf.records():
+        if rec['ADM_DR_CD'].startswith('11'):
+            record_dicts_list.append(rec.as_dict())
+
+records_df = pd.DataFrame(record_dicts_list)
+print(records_df.describe())
+exit(0)
+
+duplicate_nm_df = records_df[records_df.duplicated('EMD_KOR_NM')]
+# print(duplicate_nm_df)
+dup_df = records_df[records_df['EMD_KOR_NM'].isin(duplicate_nm_df['EMD_KOR_NM'])]
+print(dup_df)
+
+exit(0)
+
+# GIS_developer_대한민국최신행정구역
+shp_path = "E:/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
 shp_path = "/home/jy/Downloads/Datasets/Administrative/GIS_Developer_대한민국최신행정구역/EMD_202101/TL_SCCO_EMD.dbf"
 assert os.path.exists(shp_path)
 
@@ -14,7 +37,11 @@ with shapefile.Reader(shp_path, encoding='euc-kr') as sf:
             record_dicts_list.append(rec.as_dict())
 
 records_df = pd.DataFrame(record_dicts_list)
+print(records_df.describe())
+exit(0)
+
 duplicate_nm_df = records_df[records_df.duplicated('EMD_KOR_NM')]
 # print(duplicate_nm_df)
 dup_df = records_df[records_df['EMD_KOR_NM'].isin(duplicate_nm_df['EMD_KOR_NM'])]
 print(dup_df)
+
